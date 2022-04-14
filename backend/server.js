ef95d41ab04cfb17b0475a5021610c12339017d9
N00bG1rl <403helen@gmail.com>
2022-04-14 13:46:27 +0300

    User authentication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('server')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT || 8080
+
+// Connect to db
+connectDB()
+
+const app = express()
+
+// Add middleware for sending raw json
+// Get data from body
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome' })
+})
+
+// Routes - extends main route - /api/users + userRoutes route
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))

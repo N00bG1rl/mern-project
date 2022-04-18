33bb239f904450808a53fe1e0a478cc541823e7f
N00bG1rl <403helen@gmail.com>
2022-04-18 19:19:03 +0300

    Ticket functionality

---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,70 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, closeTicket, reset } from '../features/tickets/ticketSlice'
+import { useParams, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+import Spinner from '../components/layout/Spinner'
+import { BackButton } from '../components/layout/BackButton'
+
+function Ticket() {
+	const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+		state => state.tickets
+	)
+
+	const params = useParams()
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const { ticketId } = useParams()
+
+	useEffect(() => {
+		if (isError) toast.error(message)
+
+		dispatch(getTicket(ticketId))
+	}, [isError, message, ticketId])
+
+	const onTicketClose = () => {
+		dispatch(closeTicket(ticketId))
+		toast.success('Ticket closed.')
+		navigate('/tickets')
+	}
+
+	if (isLoading) {
+		return <Spinner />
+	}
+
+	if (isError) {
+		return <h3>Something went wrong.</h3>
+	}
+
+	return (
+		<div className='ticket-page'>
+			<header className='ticket-header'>
+				<BackButton url='/tickets' />
+				<h2>
+					Ticket ID: {ticket._id}
+					<span className={`status status-${ticket.status}`}>
+						{ticket.status}
+					</span>
+				</h2>
+				<h3>
+					Date submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+				</h3>
+				<h3>Product: {ticket.product}</h3>
+				<hr />
+				<div className='ticket-desc'>
+					<h3>Description of issue</h3>
+					<p>{ticket.description}</p>
+				</div>
+			</header>
+
+			{ticket.status !== 'closed' && (
+				<button className='btn btn-block btn-danger' onClick={onTicketClose}>
+					Close ticket
+				</button>
+			)}
+		</div>
+	)
+}
+
+export default Ticket

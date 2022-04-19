23d2d1ee40fddf8f631106bac124308ccd24db68
N00bG1rl <403helen@gmail.com>
2022-04-19 12:01:08 +0300

    Heroku fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const e = require('express')
 
 const PORT = process.env.PORT || 8080
 
@@ -18,9 +17,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Welcome' })
-})
+}) */
 
 // Routes - extends main route - /api/users + userRoutes route
 app.use('/api/users', require('./routes/userRoutes'))

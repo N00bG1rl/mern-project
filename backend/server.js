e4f5a3b7b707d042152b535a17c60b4624df01a8
N00bG1rl <403helen@gmail.com>
2022-04-19 11:10:43 +0300

    Heroku setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const e = require('express')
 
 const PORT = process.env.PORT || 8080
 
@@ -23,6 +25,20 @@ app.get('/', (req, res) => {
 // Routes - extends main route - /api/users + userRoutes route
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome' })
+	})
+}
 
 app.use(errorHandler)
 
